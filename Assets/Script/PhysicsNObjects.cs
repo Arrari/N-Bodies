6339097448067398918977ed268f94ecedc100f9
Arrari <arlen200@mail.ru>
2021-02-24 09:28:19 +0500

    First Release
    Initial push

---
+++ Assets/Script/PhysicsNObjects.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PhysicsNObjects : MonoBehaviour
+{
+    [SerializeField] private List<Rigidbody> obj;
+    [SerializeField] private Rigidbody rigSphere;
+    [SerializeField] private float G = 9.8f;
+    private Vector3 r;
+    private float d;
+    private RaycastHit hit;
+
+    void Start()
+    {
+        for (int i = 0; i < obj.Count; i++)
+        {
+            obj[i].useGravity = false;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                var sphere = Instantiate(rigSphere, hit.point + Vector3.back * 0.5f, Quaternion.identity);
+                obj.Add(sphere);
+                sphere.useGravity = false;
+                sphere.mass = Random.Range(1, 5);
+            }
+        }
+        for (int i = 0; i < obj.Count; i++)
+        {
+            for (int j = 0; j < obj.Count; j++)
+            {
+                if (j != i)
+                {
+                    r = obj[i].position - obj[j].position;
+
+                    d = r.magnitude;
+                    r.Normalize();
+                    obj[j].AddRelativeForce(r * G * obj[i].mass * obj[j].mass / (d * d), ForceMode.Force);
+                }
+            }
+        }
+    }
+}
